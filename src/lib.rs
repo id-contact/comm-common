17bda8dbb8da0776f0bb5eb73d3a64135118fff6
Henk Dieter Oordt <hd@oordt.dev>
2021-08-18 12:09:24 +0200

    add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,20 @@ pub mod jwt;
 pub mod session;
 pub mod types;
 pub mod util;
+
+pub mod prelude {
+    pub use crate::config::Config;
+    pub use crate::error::Error;
+    pub use crate::types::StartRequest;
+    pub use crate::util::random_string;
+    pub use crate::jwt::sign_auth_select_params;
+    #[cfg(feature = "platform_token")]
+    pub use crate::session::{Session, SessionDBConn};
+    pub use crate::types::{
+        AuthResultSet, AuthSelectParams, GuestAuthResult, 
+    };
+
+    pub use crate::types::{
+        GuestToken, HostToken, FromPlatformJwt
+    };
+}
