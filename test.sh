760a5be2a83cf8eb4429ecf2a71c7b2c52c8ebfe
Henk Dieter Oordt <hd@oordt.dev>
2021-09-09 13:18:18 +0200

    create codecov html report, unfortunately containing reports on dependencies for now

--- test.sh
+++ test.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-rustup component add llvm-tools-preview
-cargo install grcov
+# Prerequisites:
+# rustup component add llvm-tools-preview
+# cargo install grcov
+# apt install lcov
+
+export COV_OUTPUT_DIR="./target/debug/coverage"
+export LLVM_PROFILE_DIR="./target/debug/prof"
+export LLVM_PROFILE_FILE="$LLVM_PROFILE_DIR/prof-%p-%m.profraw"
 
 export RUSTFLAGS="-Zinstrument-coverage"
-cargo +nightly build
-export LLVM_PROFILE_FILE="id-contact-comm-common-%p-%m.profraw"
-rm target/debug/coverage/* || true
-cargo +nightly test
+export RUSTUP_TOOLCHAIN="nightly" # Needed for gcov and -Z Rust flag
 
-grcov . -s ./target/debug/coverage --binary-path ./target/debug/ -t lcov --branch --ignore-not-existing -o ./target/debug/coverage/
-bash <(curl -s https://codecov.io/bash) -f lcov.info
+rm -r $COV_OUTPUT_DIR/* ||  mkdir -p $COV_OUTPUT_DIR;
+rm -r $LLVM_PROFILE_DIR/* || true
+
+cargo build
+cargo test
+
+grcov -s $LLVM_PROFILE_DIR --binary-path ./target/debug/ --llvm -t lcov --branch --ignore-not-existing  -o $COV_OUTPUT_DIR/lcov.info .
+genhtml -o $COV_OUTPUT_DIR --show-details --highlight --ignore-errors source --legend $COV_OUTPUT_DIR/lcov.info
+
+# xdg-open $COV_OUTPUT_DIR/index.html
+echo "Done!"
