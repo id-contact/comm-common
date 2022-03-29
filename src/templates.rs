07b049254a2ab89b1454d9a6b0f87ce46bfe165f
Marlon Baeten <marlon@tweedegolf.com>
2022-03-29 11:21:28 +0200

    Added not_found template

--- src/templates.rs
+++ src/templates.rs
@@ -74,6 +74,7 @@ lazy_static! {
         include_template!(tera, "credentials.html");
         include_template!(tera, "login.html");
         include_template!(tera, "expired.html");
+        include_template!(tera, "not_found.html");
 
         tera
     };
